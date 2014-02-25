1c03b919d1049c1ebf0a488ddeda0008f921504d
Derrick <derrick.ongmt@gmail.com>
2014-02-25 12:52:34 +0800

    Ver 1.00
    Initial Commit

---
+++ src/consortium/psd/Timetable/Main.java
@@ -0,0 +1,15 @@
+package consortium.psd.Timetable;
+
+import consortium.psd.Controller.TimetableController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TimetableController tc = new TimetableController();
+		
+		tc.viewTt();
+		tc.viewTt(4);
+	}
+
+}
